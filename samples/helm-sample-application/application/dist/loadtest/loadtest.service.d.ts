ec9105e08b1b655e42eaf2f2acaab089afd0e319
I519840 <tom.frenken@sap.com>
2021-12-10 11:41:33 +0100

    remove dist

--- samples/helm-sample-application/application/dist/loadtest/loadtest.service.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class LoadtestService {
-    calculateExpensiveNumber(): number;
-}
--- samples/helm-sample-application/application/dist/principal-business-partner/principal-business-partner.service.d.ts
+++
@@ -1,5 +0,0 @@
-import { BusinessPartner } from '@sap/cloud-sdk-vdm-business-partner-service';
-import { Request } from 'express';
-export declare class PrincipalBusinessPartnerService {
-    getFiveBusinessPartners(request: Request): Promise<BusinessPartner[]>;
-}
