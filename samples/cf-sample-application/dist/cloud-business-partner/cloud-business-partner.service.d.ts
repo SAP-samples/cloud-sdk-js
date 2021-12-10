8cc7b1391ccefd1f1353bf674ae7be8d6ac90d52
Tom Frenken <54979414+tomfrenken@users.noreply.github.com>
2021-12-10 11:38:26 +0100

    Delete samples/cf-sample-application/dist directory

--- samples/cf-sample-application/dist/cloud-business-partner/cloud-business-partner.service.d.ts
+++
@@ -1,4 +0,0 @@
-import { BusinessPartner } from '@sap/cloud-sdk-vdm-business-partner-service';
-export declare class CloudBusinessPartnerService {
-    getFiveBusinessPartners(): Promise<BusinessPartner[]>;
-}
