c40da623d7255de65ffdd7a84fca1831421b81c0
deekshas8 <deeksha.sinha@sap.com>
2022-05-02 13:35:09 +0200

    update dest url

--- samples/getting-started/src/business-partners/business-partners.service.ts
+++ samples/getting-started/src/business-partners/business-partners.service.ts
@@ -9,11 +9,7 @@ export class BusinessPartnersService {
   async getAllBusinessPartners(): Promise<BusinessPartner[]> {
     const { businessPartnerApi } = businessPartnerService();
     return await businessPartnerApi.requestBuilder().getAll().execute({
-      url: 'https://my300098.s4hana.ondemand.com:443/',
-      username: 'ODATA_CALLER',
-      password: 'ODATA_CALLER',
-      //   username: 'CCT_E2E_TEST_USER_SDK',
-      //   password: 'CCT_E2E_TEST_USER_SDK',
+      url: 'http://localhost:3000/',
     });
   }
 }
