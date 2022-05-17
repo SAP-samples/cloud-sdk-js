852024fe43b41473d23c7b1d8ca0b3ad0fe444d6
d068544 <frank.essenberger@sap.com>
2022-05-17 14:27:39 +0200

    address manager docs
    Signed-off-by: d068544 <frank.essenberger@sap.com>

--- samples/address-manager/src/business-partner.service.ts
+++ samples/address-manager/src/business-partner.service.ts
@@ -11,7 +11,7 @@ const { businessPartnerApi, businessPartnerAddressApi } =
   businessPartnerService();
 
 /**
- * Service implementation of API.
+ * Service implementation of address API.
  */
 @Injectable()
 export class BusinessPartnerService {
@@ -33,6 +33,51 @@ export class BusinessPartnerService {
         )
       )
       .filter(businessPartnerApi.schema.BUSINESS_PARTNER_CATEGORY.equals("1"))
+      .execute({ url: "http://localhost:3000" });
+  }
+
+  /**
+   * Get a business partner by ID.
+   * @param id - Of the business partner to be returned.
+   * @returns The business partner with the given ID.
+   */
+  getBusinessPartnerById(id: string): Promise<BusinessPartner> {
+    return businessPartnerApi
+      .requestBuilder()
+      .getByKey(id)
+      .select(
+        businessPartnerApi.schema.BUSINESS_PARTNER,
+        businessPartnerApi.schema.FIRST_NAME,
+        businessPartnerApi.schema.LAST_NAME,
+        businessPartnerApi.schema.TO_BUSINESS_PARTNER_ADDRESS.select(
+          businessPartnerAddressApi.schema.BUSINESS_PARTNER,
+          businessPartnerAddressApi.schema.ADDRESS_ID,
+          businessPartnerAddressApi.schema.POSTAL_CODE,
+          businessPartnerAddressApi.schema.CITY_NAME,
+          businessPartnerAddressApi.schema.STREET_NAME,
+          businessPartnerAddressApi.schema.HOUSE_NUMBER
+        )
+      )
+      .execute({ url: "http://localhost:3000" });
+  }
+
+  /**
+   * Creates an address for a business partner.
+   * @param address - Address which is added to the business partner.
+   * @param id - ID of the business partner.
+   * @returns The address which was created.
+   */
+  public createAddress(
+    address: Record<string, any>,
+    id: string
+  ): Promise<BusinessPartnerAddress> {
+    const businessPartnerAddress = businessPartnerAddressApi
+      .entityBuilder()
+      .fromJson({ businessPartner: id, ...address });
+
+    return businessPartnerAddressApi
+      .requestBuilder()
+      .create(businessPartnerAddress)
       .execute({ url: "http://localhost:3000" });
   }
 
@@ -59,7 +104,7 @@ export class BusinessPartnerService {
   }
 
   /**
-   * Deletes and address of a business partner.
+   * Deletes an address of a business partner.
    * @param businessPartner - ID of the business partner to be updated.
    * @param addressId - ID of address to be deleted.
    * @returns - Void.
@@ -68,51 +113,6 @@ export class BusinessPartnerService {
     return businessPartnerAddressApi
       .requestBuilder()
       .delete(businessPartner, addressId)
-      .execute({ url: "http://localhost:3000" });
-  }
-
-  /**
-   * Creates a address for a business partner.
-   * @param address - Address which is added to the business partner.
-   * @param id - ID of the business partner.
-   * @returns The address which was created.
-   */
-  public createAddress(
-    address: Record<string, any>,
-    id: string
-  ): Promise<BusinessPartnerAddress> {
-    const businessPartnerAddress = businessPartnerAddressApi
-      .entityBuilder()
-      .fromJson({ businessPartner: id, ...address });
-
-    return businessPartnerAddressApi
-      .requestBuilder()
-      .create(businessPartnerAddress)
-      .execute({ url: "http://localhost:3000" });
-  }
-
-  /**
-   * Get a business partner by ID.
-   * @param id - Of the business partner to be returned.
-   * @returns The business partner with the given ID.
-   */
-  getBusinessPartnerById(id: string): Promise<BusinessPartner> {
-    return businessPartnerApi
-      .requestBuilder()
-      .getByKey(id)
-      .select(
-        businessPartnerApi.schema.BUSINESS_PARTNER,
-        businessPartnerApi.schema.FIRST_NAME,
-        businessPartnerApi.schema.LAST_NAME,
-        businessPartnerApi.schema.TO_BUSINESS_PARTNER_ADDRESS.select(
-          businessPartnerAddressApi.schema.BUSINESS_PARTNER,
-          businessPartnerAddressApi.schema.ADDRESS_ID,
-          businessPartnerAddressApi.schema.POSTAL_CODE,
-          businessPartnerAddressApi.schema.CITY_NAME,
-          businessPartnerAddressApi.schema.STREET_NAME,
-          businessPartnerAddressApi.schema.HOUSE_NUMBER
-        )
-      )
       .execute({ url: "http://localhost:3000" });
   }
 }
